4bcdf3939b871016926d50cdacb11eecc7f6ad40
Adam <55118346+Jman24@users.noreply.github.com>
2019-11-26 18:55:50 -0500

    Added publish functions for buttons/slider

--- R2-D2/ViewController.swift
+++ R2-D2/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import CocoaMQTT
 
 class ViewController: UIViewController {
+    
+    let mqttClient = CocoaMQTT(clientID: "Adam's iPhone", host: "192.168.1.13", port: 1883)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,16 +20,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func mqtt_connect(_ sender: UIButton) {
+        mqttClient.connect()
     }
     @IBAction func mqtt_disconnect(_ sender: UIButton) {
+        mqttClient.disconnect()
     }
     
     @IBAction func drive_forward(_ sender: UIButton) {
+        if sender.isSelected{
+            mqttClient.publish("rpi/manual_fwd", withString: "fwd")
+        }
+        else{
+            mqttClient.publish("rpi/manual_fwd", withString: "not_fwd")
+        }
     }
     @IBAction func drive_backwards(_ sender: UIButton) {
+        if sender.isSelected{
+            mqttClient.publish("rpi/manual_back", withString: "back")
+        }
+        else{
+            mqttClient.publish("rpi/manual_back", withString: "not_back")
+        }
     }
     
     @IBAction func turn_left_right(_ sender: UISlider) {
+        //todo: snap back to zero when untouched
+        mqttClient.publish("rpi/manual_back", withString: String(sender.value))
     }
     
     /*
