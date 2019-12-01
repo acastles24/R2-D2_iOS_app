7e855afe453c7b239a910e9a6bfb8055737104ce
Adam <55118346+Jman24@users.noreply.github.com>
2019-12-01 15:19:13 -0500

    Created joystick

--- R2-D2/manualControlScene.swift
+++ R2-D2/manualControlScene.swift
@@ -6,8 +6,27 @@
 //  Copyright © 2019 Adam Castles. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
 
 class manualControlScene: SKScene {
-
+    
+    lazy var joystick: TLAnalogJoystick = {
+        let jstick = TLAnalogJoystick(withDiameter: 100)
+        jstick.position = CGPoint(x: UIScreen.main.bounds.size.width * -0.5 + jstick.radius + 45, y: UIScreen.main.bounds.size.height * -0.5 + jstick.radius + 45)
+        return jstick
+    }()
+    
+    func setup(){
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        setupJoystick()
+    }
+    
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+    
+    func setupJoystick(){
+        addChild(joystick)
+        
+    }
 }
